5624bb7e4d9dcf5ba3d4a7d3cf7e8d740f2f2541
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-11 23:34:59 -0500

    logged current metric for oil temp

--- src/Features/Oil/OilChart.tsx
+++ src/Features/Oil/OilChart.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Provider, createClient, useQuery } from 'urql';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const client = createClient({
   url: 'https://react.eogresources.com/graphql',
@@ -8,9 +9,10 @@ const client = createClient({
 const query = `
 {
   getLastKnownMeasurement(metricName: "oilTemp") {
-    value
-    at
     metric
+    value
+    unit
+    at
   }
 }
 `;
@@ -31,9 +33,25 @@ const OilChart = () => {
   const { fetching, data, error } = result;
   console.log(result);
 
+  useEffect(() => {
+    if (error) {
+      return;
+    }
+
+    if (!data) return;
+
+    const { getWeatherForLocation } = data;
+
+  }, [ data, error]);
+
+  if (fetching) return <LinearProgress />;
+
   return (
     <div>
-      <h1>Oil Chart</h1>
+      <h1>{data.getLastKnownMeasurement.metric}</h1>
+      <p>{data.getLastKnownMeasurement.value}</p>
     </div>
   )
-}
+};
+
+  
